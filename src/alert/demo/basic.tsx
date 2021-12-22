e8d8df157a4d8622a51e401f436914605d547e1a
李森 <lisenbd@chanjet.com>
2021-12-22 18:23:59 +0800

    test

--- src/alert/demo/basic.tsx
+++ src/alert/demo/basic.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import Alert from '../index';
 import '../style';
 
-export default () => <Alert kind="warning">这是一条警告提示</Alert>;
+export default () => <Alert kind="warning">这是一条warning警告提示</Alert>;
